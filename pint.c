816183b058209c3d2a0d624a4e3429ea2bf38218
Erkenoss <xavier.bertin6@gmail.com>
2023-09-06 14:37:39 +0000

    fixed return line pint.c

--- pint.c
+++ pint.c
@@ -4,7 +4,7 @@ void pint(stack_t **list, unsigned int line_number)
 {
     if (*list == NULL)
     {
-        fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
         exit(EXIT_FAILURE);
     }
     printf("%d\n", (*list)->n);
